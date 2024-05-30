bdeec066918f53b9c288973a5758643cf28bb87f
happynumb-wh <wanghan192@mails.ucas.ac.cn>
2024-05-30 17:06:16 +0800

    fix(dasics-syscall-checek) Fix dasics syuscall check logic

--- src/start/fault.c
+++ src/start/fault.c
@@ -3,28 +3,6 @@
 #include <ucsr.h>
 #include <dasics_stdio.h>
 
-static long invoke_syscall(long sysno, long arg0, long arg1, long arg2,
-                           long arg3, long arg4, long arg5, long arg6)
-{
-    register long a7 asm("a7") = sysno;
-    register long a0 asm("a0") = arg0;
-    register long a1 asm("a1") = arg1;
-    register long a2 asm("a2") = arg2;
-    register long a3 asm("a3") = arg3;
-    register long a4 asm("a4") = arg4;
-    register long a5 asm("a5") = arg5;
-    register long a6 asm("a6") = arg6;
-
-    asm volatile("ecall"                      \
-                 : "+r"(a0)                   \
-                 : "r"(a1), "r"(a2), "r"(a3), \
-                   "r"(a4), "r"(a5), "r"(a6), \
-                   "r"(a7)                    \
-                 : "memory");
-
-    return a0;
-}
-
 void dasics_start_fault(struct ucontext_trap * regs)
 {
 
@@ -49,18 +27,11 @@ void dasics_start_fault(struct ucontext_trap * regs)
         break;
 
     case EXC_DASICS_UECALL_FAULT:
-        regs->a0 = invoke_syscall(regs->a7, \
-                                    regs->a0, \
-                                    regs->a1, \
-                                    regs->a2, \
-                                    regs->a3, \
-                                    regs->a4, \
-                                    regs->a5, \
-                                    regs->a6);
-
-
         // jump ecall
-        regs->uepc += 4;
+        regs->uepc += 4;    
+        regs->a0 = invoke_syscall(regs);
+
+        //unreachable here
         break;
 
     default:
