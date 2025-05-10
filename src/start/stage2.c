85c48bcaf1b0a93a04021d2ce065b372578f7f71
Chengyuan Zhang <1136427613@qq.com>
2025-05-10 11:10:51 +0800

    change to realse

--- src/start/stage2.c
+++ src/start/stage2.c
@@ -101,8 +101,8 @@ void _dasics_entry_stage2(uint64_t sp, rtld_fini fini)
     // setup user ufault handler 
     csr_write(0x005, (uint64_t)dasics_ufault_entry);
 
-    atexit(&print_exit_func_num);
-    _umain_elf_table->calculate = 1;
+    // atexit(&print_exit_func_num);
+    // _umain_elf_table->calculate = 1;
 
 #endif
 
